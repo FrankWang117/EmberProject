1552a5e1a4b634737ce21a268d1354abbd29634b
Frank <wsgoodman@163.com>
2018-11-16 21:26:49 +0800

    action up 与model 操作过程中model的一些属性值变化。

--- ember-guide/app/modules/components/lj-ershoufang/component.js
+++ ember-guide/app/modules/components/lj-ershoufang/component.js
@@ -1,7 +1,12 @@
 import Component from "@ember/component";
 
 export default Component.extend({
-	localClassNames: "lj-ershoufang",
-	classNames: ["ershoufang-list"],
-	tagName: "li"
+    localClassNames: "lj-ershoufang",
+    classNames: ["ershoufang-list"],
+    tagName: "li",
+    actions: {
+        increaseTotalPrice(id) {
+            this.sendAction("increaseTotalPrice", id);
+        }
+    }
 });
---
+++ ember-guide/app/modules/how-generate-component/controller.js
@@ -0,0 +1,33 @@
+import Controller from "@ember/controller";
+
+export default Controller.extend({
+	actions: {
+		increaseTP(id) {
+			let ershoufang = this.store.peekRecord("ershoufang", id);
+			let newTP = ershoufang.incrementProperty("totalprice");
+			let dirtyAttributes = ershoufang.get("hasDirtyAttributes");
+			let changedAttributes = ershoufang.changedAttributes();
+			let objectAttributes = {
+				newTP: newTP,
+				dirtyAttributes: dirtyAttributes,
+				changedAttributes: changedAttributes
+			};
+
+			console.log(changedAttributes);
+			this.set("objectAttributes", objectAttributes);
+		},
+		deleteRecord() {
+			let ershoufang1 = this.store.peekRecord("ershoufang", "ershoufang01");
+			/**
+			 * Record.deleteRecord();
+			 */
+
+			ershoufang1.deleteRecord();
+			let isDeleted = ershoufang1.get("isDeleted");
+
+			console.log(isDeleted);
+			ershoufang1.save();
+
+		}
+	}
+});
